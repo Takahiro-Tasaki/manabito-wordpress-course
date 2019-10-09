ffbd66d52e2ba1ac7b198646e41b601ef349d796
田崎貴大 <qxgxyea_h@yahoo.co.jp>
2019-10-09 11:23:11 +0900

    2019.10.08 commit.

---
+++ standard/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <title>スタンダードレイアウト</title>
+  <link rel="stylesheet" href="/tasaki/wp-content/themes/standard/css/reset.css">
+  <link rel="stylesheet" href="/tasaki/wp-content/themes/standard/css/style.css">
+</head>
+<body>
+  <header class="header">
+    <h1 class="logo">
+      <a href="/">SAMPLE SITE</a>
+    </h1>
+    <nav class="global-nav">
+      <ul>
+        <li class="nav-item active"><a href="#">HOME</a></li>
+        <li class="nav-item"><a href="#">ABOUT</a></li>
+        <li class="nav-item"><a href="#">NEWS</a></li>
+        <li class="nav-item"><a href="#">TOPICS</a></li>
+        <li class="nav-item"><a href="#">DOCS</a></li>
+        <li class="nav-item"><a href="#">BLOG</a></li>
+      </ul>
+    </nav>
+  </header>
