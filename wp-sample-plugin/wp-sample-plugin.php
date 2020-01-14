94064e07c200e406754da61fcdfc7aea1720722c
garage <garage@garagenoiMac.local>
2020-01-14 21:05:32 +0900

    2020.01.14 commit.

--- wp-sample-plugin/wp-sample-plugin.php
+++ wp-sample-plugin/wp-sample-plugin.php
@@ -1,62 +1,35 @@
 <?php
 /*
-Plugin Name: wp-sample-plugin
+Plugin Name: Sample Plugin
 Plugin URI: 
 Description: プラグインの説明
 Version: 1.0.0
-Author: 作者名
+Author: 作成者
 Author URI: 
 License: GPLv2 or later
 */
-class Test {
-  public $str;
-  
-  public function __construct( $a ) {
-    $this->str = $a;
-  }
-  
-  public function test_one() {
-    echo $this->str . '<br>';
-  }
-  
-  public function test_two() {
-    echo $this->str . ' ' . $this->str . '<br>';
-  }
+
+/**
+ * Base Class
+ *
+ * @author  作成者
+ * @version 1.0.0
+ * @since   1.0.0
+ */
+class Sample_Plugin {
+	public function __construct() {
+		add_action( 'the_title', array( $this, 'set_title' ) );
+	}
+	
+	/**
+	 * タイトルのセッティング
+	 *
+	 * @param  string $title
+	 * @return string
+	 */
+	public function set_title( $title ) {
+		return '★' . $title;
+	}
 }
-$test = new Test( 'クラスの引数' );
-$test->test_one();
-$test->test_two();
 
-class Human {
-  public $para = array();
-
-  public function __construct( $a ) {
-    $this->para = $a;
-  }
-  
-  public function greet( $name ) {
-    echo $name . 'さん、' . $this->para['greet'] . '<br>';
-  }
-  
-  public function height() {
-    echo $this->para['height'] . '<br>';
-  }
-}
-$jp_para = array(
-  'name'   => '山田太郎',
-  'greet'  => 'こんばんは',
-  'height' => 170
-);
-$human_jp = new Human( $jp_para );
-
-$usa_para = array(
-  'name'   => 'Mike',
-  'greet'  => 'GoodEvening',
-  'height' => 190
-);
-$human_usa = new Human( $usa_para );
-
-$human_jp->greet( $human_usa->para['name'] );
-$human_jp->height();
-$human_usa->greet( $human_jp->para['name'] );
-$human_usa->height();
+new Sample_Plugin();
